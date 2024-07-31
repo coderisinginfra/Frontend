8f437b3cd69768c0cb4aa293b1d5d1cb9f3e80de
coderisinginfra <risinginfranoida@gmail.com>
2024-07-31 17:26:28 +0530

    Update HomePage.jsx

--- src/websitepage/HomePage.jsx
+++ src/websitepage/HomePage.jsx
@@ -132,6 +132,16 @@ useEffect(()=>{
     setOpen(false);
   };
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.id = 'pixel-script-poptin';
+    script.src = 'https://cdn.popt.in/pixel.js?id=c466aa9a21a37';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // No cleanup needed for adding the script
+  }, []);
+
 
 
   return (

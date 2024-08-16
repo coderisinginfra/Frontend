7b32378bda62d0311f6bbd14a559f9e3d672871e
sudhanshu <risinginfranoida@gmail.com>
2024-08-16 13:38:51 +0530

    add

--- src/websitepage/Attraction.jsx
+++ src/websitepage/Attraction.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Attraction = () => {
+const Attraction = (props) => {
+    useEffect(()=>{
+        document.title = props.title
+    },[props.title])
   return (
     <div>
       <a href="https://www.nextbizthing.com/noida/real-estate/risinginfra?from=badge"  title="Find me on NextBizThing" target="_blank"><img src="https://www.nextbizthing.com/images/memberbadge.png" style="border: none;"/></a>

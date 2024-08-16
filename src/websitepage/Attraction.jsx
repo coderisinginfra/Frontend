2f7ff624afafac8c7fd749b03a8ebbf47c81494f
sudhanshu <risinginfranoida@gmail.com>
2024-08-16 14:05:21 +0530

    add

--- src/websitepage/Attraction.jsx
+++ src/websitepage/Attraction.jsx
@@ -1,15 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 const Attraction = (props) => {
-    useEffect(()=>{
-        document.title = props.title
-    },[props.title])
+  useEffect(() => {
+    if (props.title) {
+      document.title = props.title;
+    }
+  }, [props.title]);
+
   return (
     <div>
-      <a href="https://www.nextbizthing.com/noida/real-estate/risinginfra?from=badge"  title="Find me on NextBizThing" target="_blank"><img src="https://www.nextbizthing.com/images/memberbadge.png" style="border: none;"/></a>
-                        
+      <a 
+        href="https://www.nextbizthing.com/noida/real-estate/risinginfra?from=badge"  
+        title="Find me on NextBizThing" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <img 
+          src="https://www.nextbizthing.com/images/memberbadge.png" 
+          alt="NextBizThing Badge" 
+          style={{ border: 'none' }}
+        />
+      </a>                      
     </div>
-  )
-}
+  );
+};
 
-export default Attraction
+export default Attraction;

36fcaa99e1314fe0e2cd412ba94d272ce9d28868
sudhanshu <risinginfranoida@gmail.com>
2024-08-17 10:24:37 +0530

    add

--- src/pages/AllListingShow.jsx
+++ src/pages/AllListingShow.jsx
@@ -39,6 +39,8 @@ const AllListingShow = () => {
         const response = await axios.get(`https://www.backend.risinginfra.in/api/v1/fetchalllistings/${propertyTitle}`);
         setPropertyData(response.data.data);
         setLoading(false);
+        console.log(propertyData.description)
+        console.log(propertyData.keywords)
       } catch (error) {
         setError(error.message);
         setLoading(false);

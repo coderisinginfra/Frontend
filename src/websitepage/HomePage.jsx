57ffb54e7c603a9bd260a925c8f9c864028a5c04
sudhanshu <risinginfranoida@gmail.com>
2024-09-17 14:11:59 +0530

    add

--- src/websitepage/HomePage.jsx
+++ src/websitepage/HomePage.jsx
@@ -141,11 +141,7 @@ useEffect(()=>{
   </Helmet>
      <ToastContainer style={{marginTop:"7em"}}/>
    <div className='container-home'>
-<<<<<<< HEAD
-    {/* <Dialog
-=======
      {/* <Dialog
->>>>>>> 5c20ac1115c1e29cf866946217c25f39beb21ca9
         open={open}
         onClose={handleClose}
         PaperProps={{
@@ -211,11 +207,8 @@ useEffect(()=>{
           <Button type="submit" variant='contained' color='primary' onClick={handleSubmit} disabled={isButtonDisabled}>Submit</Button>
           <Button onClick={handleClose}>Cancel</Button>
         </DialogActions>
-<<<<<<< HEAD
       </Dialog>  */}
-=======
-      </Dialog> */}
->>>>>>> 5c20ac1115c1e29cf866946217c25f39beb21ca9
+
       <div> 
         <h1 className='headiing-home-fron-container'>Find Your Property <br />With Rising Infra</h1>
         <p className='heading-image paragraph-home-front-data'>Discover your dream property nestled in the heart of serene surroundings, offering unparalleled luxury and comfort.</p>

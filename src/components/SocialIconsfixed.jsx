4e164f97c0041b4995a857a84fa3087e44a30e9a
sudhanshu <risinginfranoida@gmail.com>
2024-11-06 19:27:38 +0530

    add

--- src/components/SocialIconsfixed.jsx
+++ src/components/SocialIconsfixed.jsx
@@ -5,9 +5,9 @@ const SocialIconsfixed = () => {
   return (
     <div class="social-container">
         <ul class="social-icons">
-            <li><a target='_main' href="https://www.instagram.com/rising_infra/"><i class="fa fa-instagram"></i></a></li> <br />
-            <li><a target='_main' href="https://www.facebook.com/risinginfranoida"><i class="fa fa-facebook"></i></a></li> <br />
-            <li><a target='_main' href="https://www.linkedin.com/company/risinginfra/"><i class="fa fa-linkedin"></i></a></li> <br />
+            <li><a target='_main' href="https://www.instagram.com/rising_infra/" aria-label='instagram'><i class="fa fa-instagram" ></i></a></li> <br />
+            <li><a target='_main' href="https://www.facebook.com/risinginfranoida" aria-label='facebooj'><i class="fa fa-facebook"></i></a></li> <br />
+            <li><a target='_main' href="https://www.linkedin.com/company/risinginfra/" aria-label='linkedin'><i class="fa fa-linkedin"></i></a></li> <br />
         </ul>
     </div>
   )

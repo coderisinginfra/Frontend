5015ad6982221e7dc43608fcdd57ace6d47c51c1
sudhanshu <risinginfranoida@gmail.com>
2024-11-07 11:03:10 +0530

    add

--- src/websitepage/HomePage.jsx
+++ src/websitepage/HomePage.jsx
@@ -247,7 +247,7 @@ useEffect(()=>{
           <div key={index} className='data-front-home'>
               <Link to={`/projects/${item.propertyTitle.replace(/ /g, '-').toLowerCase()}`} className='links-data-home'>
                 <div>
-                  <img src={item.images} alt="images" className='images-listing' sizes='(max-width: 768px)50vw,(max-width:1200px)50vw,33vw'/>
+                  <img src={item.images} alt="images" className='images-listing' />
                   <h1 className='title-home'>{item.propertyTitle}</h1>
                   <div className='icons-image-home'>
                 <LocationOnIcon style={{color:"green"}}/>

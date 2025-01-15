2263686a89f97f78861830a5b7ddd9d2107cf84f
sudhanshu <risinginfranoida@gmail.com>
2025-01-15 14:13:49 +0530

    add

---
+++ src/components/Whatsappicons.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../CSS/wicons.css'
+
+const Whatsappicons = () => {
+  return (
+    <div>
+    <>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <a
+          href="https://wa.me/+919990633353"
+          className="whatsapp"
+          target="_blank"
+        >
+          {" "}
+          <i className="fa fa-whatsapp whatsapp-icon" />
+         </a>
+</>
+
+    </div>
+  )
+}
+
+export default Whatsappicons
